a4110eeb1addc86db7ae353804d93c399f123687
Nathan Clark <Nathan.Clark@tokenshift.com>
2012-11-21 19:21:39 -0500

    Some refactoring to allow alternate names for options and flags, and unit tests.

--- args.go
+++ args.go
@@ -16,48 +16,37 @@ func Args(args []string) (parser Expectation) {
 	return exp
 }
 
-/* Represents the result of parsing and validating
- * a set of command-line arguments.
-
- * Provides methods to access specific parameter, option
- * and flag values. */
-type ArgResult interface {
-	/* Gets whether the named flag was set.
-	 * name: The name of the flag to check. */
-	Flag(name string) (value bool, err error)
-
-	/* Gets the value of the parameter at the specified position.
-	 * i: The 0-based index of the parameter. */
-	ParamAt(i int) (param string, err error)
-
-	/* Gets the value of the named parameter.
-	 * name: The name of the parameter. */
-	ParamNamed(name string) (param string, err error)
-}
-
-type argResult struct {
-}
-
-
 /* Represents a set of parse-and-validation instructions.
  *
  * Expectation methods are intended to be chained, ala jQuery.
  * The returned object may or may not be the same object as
  * the original; do not depend on either behavior! */
 type Expectation interface {
+	/* Allowances */
+	// These argments MAY be present.
+
 	/* Consumes a single flag from the command-line arguments, if found.
 	 *
-	 * name: The name of the flag to look for. */
-	AllowFlag(name string) (chain Expectation)
+	 * name: The name of the flag to look for.
+	 * alts: Any other names that could be used to refer to the same option,
+	 * including single-character names. Any single-character names should
+	 * appear in the form "-n".
+	 *
+	 * The flag can only be accessed by its name or position, not by
+	 * any of the alternate names. */
+	AllowFlag(name string, alts ...string) (chain Expectation)
 
 	/* Consumes a single option and its value from the command-line arguments,
 	 * if found.
 	 *
 	 * name: The name of the option, as it will appear in the form "--name".
-	 * alt: The short/single-character name of the option, as it will appear
-	 * in the form "-n".
-	 * At least one of the arguments must be non-nil. */
-	AllowOption(name string, alt string) (chain Expectation)
+	 * alts: Any other names that could be used to refer to the same option,
+	 * including single-character names. Any single-character names should
+	 * appear in the form "-n".
+	 *
+	 * The option can only be accessed by its name or position, not by
+	 * any of the alternate names. */
+	AllowOption(name string, alts ...string) (chain Expectation)
 
 	/* Consumes the next argument from the command-line as a parameter.
 	 * 
@@ -68,23 +57,33 @@ type Expectation interface {
 	 * giving it the specified name.
 	 * 
 	 * If there are no more arguments to consume, nothing will be consumed,
-	 * and the named parameter will not be present in the ArgResult.
+	 * and the named parameter will not be present in the result.
 	 * name: The name that will be assigned to the parameter. */
 	AllowNamedParam(name string) (chain Expectation)
 
+
+	/* Expectations */
+	// These argments MUST be present.
+
 	/* Consumes a single flag from the command-line arguments.
 	 * The flag must be present, otherwise validation will fail.
-	 * name: The name of the flag to look for. */
-	ExpectFlag(name string) (chain Expectation)
+	 * name: The name of the flag to look for.
+	 *
+	 * The flag can only be accessed by its name or position, not by
+	 * any of the alternate names. */
+	ExpectFlag(name string, alts ...string) (chain Expectation)
 
 	/* Consumes a single option and its value from the command-line arguments.
 	 *
 	 * If the option is not found, validation will fail.
 	 * name: The name of the option, as it will appear in the form "--name".
-	 * alt: The short/single-character name of the option, as it will appear
-	 * in the form "-n".
-	 * At least one of the arguments must be non-nil. */
-	ExpectOption(name string, alt string) (chain Expectation)
+	 * alts: Any other names that could be used to refer to the same option,
+	 * including single-character names. Any single-character names should
+	 * appear in the form "-n".
+	 *
+	 * The option can only be accessed by its name or position, not by
+	 * any of the alternate names. */
+	ExpectOption(name string, alts ...string) (chain Expectation)
 
 	/* Consumes the next argument from the command-line as a parameter.
 	 *
@@ -98,6 +97,9 @@ type Expectation interface {
 	 * name: The name that will be assigned to the parameter. */
 	ExpectNamedParam(name string) (chain Expectation)
 
+
+	/* Termination */
+
 	/* Discards any remaining, unconsumed arguments, so that they will
 	 * not cause validation to fail.
 	 *
@@ -105,11 +107,30 @@ type Expectation interface {
 	Chop() (chain Expectation)
 
 	/* Discards any remaining, unconsumed arguments and calls Validate. */
-	ChopAndValidate() (result ArgResult, err error)
+	ChopAndValidate() (result Expectation, err error)
 
 	/* Called once all expectations have been specified, to parse and
 	 * validate the arguments. */
-	Validate() (result ArgResult, err error)
+	Validate() (result Expectation, err error)
+
+	
+	/* Results */
+
+	/* Gets whether the named flag was set.
+	 * name: The name of the flag to check. */
+	Flag(name string) (value bool, err error)
+
+	/* Gets the value of the named option.
+	 * name: The name of the option. */
+	Option(name string) (value string, err error)
+
+	/* Gets the value of the parameter at the specified position.
+	 * i: The 0-based index of the parameter. */
+	ParamAt(i int) (param string, err error)
+
+	/* Gets the value of the named parameter.
+	 * name: The name of the parameter. */
+	ParamNamed(name string) (param string, err error)
 }
 
 /* Implementation of the Expectation interface. */
@@ -123,7 +144,7 @@ type processor func(exp expectation) (err error)
 /* Consumes a single flag from the command-line arguments, if found.
  *
  * name: The name of the flag to look for. */
-func (old expectation) AllowFlag(name string) (Expectation) {
+func (old expectation) AllowFlag(name string, alts ...string) (Expectation) {
 	old.processors = append(old.processors, func(exp expectation) (err error) {
 		return
 	})
@@ -134,10 +155,13 @@ func (old expectation) AllowFlag(name string) (Expectation) {
  * if found.
  *
  * name: The name of the option, as it will appear in the form "--name".
- * alt: The short/single-character name of the option, as it will appear
- * in the form "-n".
- * At least one of the arguments must be non-nil. */
-func (old expectation) AllowOption(name string, alt string) (Expectation) {
+ * alts: Any other names that could be used to refer to the same option,
+ * including single-character names. Any single-character names should
+ * appear in the form "-n".
+ *
+ * The option can only be accessed by its name or position, not by
+ * any of the alternate names. */
+func (old expectation) AllowOption(name string, alts ...string) (Expectation) {
 	old.processors = append(old.processors, func(exp expectation) (err error) {
 		return
 	})
@@ -158,7 +182,7 @@ func (old expectation) AllowParam() (Expectation) {
  * giving it the specified name.
  * 
  * If there are no more arguments to consume, nothing will be consumed,
- * and the named parameter will not be present in the ArgResult.
+ * and the named parameter will not be present in the result.
  * name: The name that will be assigned to the parameter. */
 func (old expectation) AllowNamedParam(name string) (Expectation) {
 	old.processors = append(old.processors, func(exp expectation) (err error) {
@@ -170,7 +194,7 @@ func (old expectation) AllowNamedParam(name string) (Expectation) {
 /* Consumes a single flag from the command-line arguments.
  * The flag must be present, otherwise validation will fail.
  * name: The name of the flag to look for. */
-func (old expectation) ExpectFlag(name string) (Expectation) {
+func (old expectation) ExpectFlag(name string, alts ...string) (Expectation) {
 	old.processors = append(old.processors, func(exp expectation) (err error) {
 		return
 	})
@@ -181,10 +205,13 @@ func (old expectation) ExpectFlag(name string) (Expectation) {
  *
  * If the option is not found, validation will fail.
  * name: The name of the option, as it will appear in the form "--name".
- * alt: The short/single-character name of the option, as it will appear
- * in the form "-n".
- * At least one of the arguments must be non-nil. */
-func (old expectation) ExpectOption(name string, alt string) (Expectation) {
+ * alts: Any other names that could be used to refer to the same option,
+ * including single-character names. Any single-character names should
+ * appear in the form "-n".
+ *
+ * The option can only be accessed by its name or position, not by
+ * any of the alternate names. */
+func (old expectation) ExpectOption(name string, alts ...string) (Expectation) {
 	old.processors = append(old.processors, func(exp expectation) (err error) {
 		return
 	})
@@ -225,14 +252,38 @@ func (old expectation) Chop() (Expectation) {
 }
 
 /* Discards any remaining, unconsumed arguments and calls Validate. */
-func (final expectation) ChopAndValidate() (result ArgResult, err error) {
+func (final expectation) ChopAndValidate() (result Expectation, err error) {
 	err = fmt.Errorf("Not yet implemented.")
+	result = final
 	return
 }
 
 /* Called once all expectations have been specified, to parse and
  * validate the arguments. */
-func (final expectation) Validate() (result ArgResult, err error) {
+func (final expectation) Validate() (result Expectation, err error) {
 	err = fmt.Errorf("Not yet implemented.")
+	result = final
+	return
+}
+
+func (final expectation) Flag(name string) (value bool, err error) {
+	err = fmt.Errorf("Not yet implemented.")
+	return
+}
+
+func (final expectation) Option(name string) (value string, err error) {
+	err = fmt.Errorf("Not yet implemented.")
+	return
+}
+
+func (final expectation) ParamAt(index int) (value string, err error) {
+	err = fmt.Errorf("Not yet implemented.")
+	value = ""
+	return
+}
+
+func (final expectation) ParamNamed(name string) (value string, err error) {
+	err = fmt.Errorf("Not yet implemented.")
+	value = ""
 	return
 }
--- args_test.go
+++ args_test.go
@@ -4,39 +4,134 @@ import (
 	"testing"
 )
 
-func TestDoAddWithInit(t *testing.T) {
-	result, err := Args([]string {"do", "add", "\"this is a test\"", "--init"}).
+func TestSingleFlag(t *testing.T) {
+	t.Error("Test not implemented.")
+}
+
+func TestSingleMissingFlag(t *testing.T) {
+	t.Error("Test not implemented.")
+}
+
+func TestSingleOption(t *testing.T) {
+	t.Error("Test not implemented.")
+}
+
+func TestSingleAltOption(t *testing.T) {
+	t.Error("Test not implemented.")
+}
+
+func TestSingleShortAltOption(t *testing.T) {
+	t.Error("Test not implemented.")
+}
+
+func assertFlag(t *testing.T, args Expectation, name string, expected bool) {
+	val, err := args.Flag(name)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if val != expected {
+		if expected {
+			t.Errorf("Expected flag '%v'.", name)
+		} else {
+			t.Errorf("Did not expect flag '%v'.", name)
+		}
+	}
+}
+
+func assertOption(t *testing.T, args Expectation, name string, expected string) {
+	val, err := args.Option(name)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if val != expected {
+		t.Errorf("Expected option named '%v' to be '%v'.", name, expected)
+	}
+}
+
+func assertParamAt(t *testing.T, args Expectation, index int, expected string) {
+	val, err := args.ParamAt(index)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if val != expected {
+		t.Errorf("Expected parameter at position %v to be '%v'.", index, expected)
+	}
+}
+
+func assertParamNamed(t *testing.T, args Expectation, name string, expected string) {
+	val, err := args.ParamNamed(name)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if val != expected {
+		t.Errorf("Expected parameter named '%v' to be '%v'.", name, expected)
+	}
+}
+
+func TestSingleParameter(t *testing.T) {
+	result, err := Args([]string {"test"}).
 		ExpectParam().
-		ExpectNamedParam("command").
-		ExpectNamedParam("title").
-		AllowFlag("init").
 		Validate()
 
 	if err != nil {
 		t.Error(err)
+		return
 	}
 
-	if val, err := result.ParamAt(0); err != nil || val != "do" {
-		t.Error("Expected 'do' as first param.")
-	}
-
-	if val, err := result.ParamAt(1); err != nil || val != "add" {
-		t.Error("Expected 'add' as second param.")
-	}
-
-	if val, err := result.ParamNamed("command"); err != nil || val != "add" {
-		t.Error("Expected 'add' as command.")
-	}
-
-	if val, err := result.ParamAt(2); err != nil || val != "\"this is a test\"" {
-		t.Error("Expected \"this is a test\" as third param.")
-	}
-
-	if val, err := result.ParamNamed("title"); err != nil || val != "\"this is a test\"" {
-		t.Error("Expected \"this is a test\" as title.")
-	}
-
-	if val, err := result.Flag("init"); err != nil || !val {
-		t.Error("Expected 'init' to be true.")
-	}
+	assertParamAt(t, result, 0, "test")
 }
+
+func TestSingleNamedParameter(t *testing.T) {
+	result, err := Args([]string {"test"}).
+		ExpectNamedParam("key").
+		Validate()
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	assertParamNamed(t, result, "key", "test")
+}
+
+func TestMultipleArguments(t *testing.T) {
+	result, err := Args([]string {
+		"do",
+		"something",
+		"--carefully",
+		"--and",
+		"slowly",
+		"-v",
+		"(that means verbose)"}).
+		ExpectParam().
+		ExpectNamedParam("command").
+		ExpectFlag("carefully").
+		ExpectOption("and").
+		ExpectOption("verbose", "v").
+		Validate()
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	assertParamAt(t, result, 0, "do")
+	assertParamNamed(t, result, "command", "something")
+	assertFlag(t, result, "carefully", true)
+	assertOption(t, result, "and", "slowly")
+	assertOption(t, result, "verbose", "(that means verbose)")
+}
+
+// TODO: Add negative tests.
